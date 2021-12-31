0faf774f43d7a6f1144536cb2655a11b21993f4f
Marcin Wojdat <wojdatm98@gmail.com>
2021-12-31 17:23:22 +0100

    Rozszerza edytor o testcasy

--- src/components/tabs/CodeTab.vue
+++ src/components/tabs/CodeTab.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="full-size">
     <CodeEditor id="code" :readonly="false" />
+    <TestCase />
   </div>
 </template>
 
 <script>
 import CodeEditor from '@/components/codeEditor/CodeEditor.vue'
+import TestCase from '@/components/testCase/TestCase.vue'
 export default {
-  components: { CodeEditor }
+  components: { CodeEditor, TestCase }
 }
 </script>
 
 <style scoped>
+  .code-editor {
+    height: 70%;
+  }
   
+  .test-case {
+    height: 30%;
+  }
 </style>
