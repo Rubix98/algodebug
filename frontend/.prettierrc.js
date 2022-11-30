44ef89cabdafc423dc99727b62ed737c4589f65f
Jakub Pawłowski <59766830+pawl0wski@users.noreply.github.com>
2022-11-30 18:09:18 +0100

    style(frontend,backend): Add comment about excluding files from formatting

--- frontend/.prettierrc.js
+++ frontend/.prettierrc.js
@@ -1,5 +1,6 @@
 // Config options: https://prettier.io/docs/en/options.html
 // Config file: https://prettier.io/docs/en/configuration.html
+// You can exclude files/folders from formatting by adding them to .prettierignore
 module.exports = {
     useTabs: false,
     tabWidth: 4,
