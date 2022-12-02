c9ed55210542ee09a699b1221e999e643dbfc9c8
Marcin Wojdat <wojdatm98@gmail.com>
2022-12-02 13:47:21 +0100

    Add OutputParser class

--- backend/src/services/codeservice.ts
+++ backend/src/services/codeservice.ts
@@ -1,10 +1,5 @@
-import { CodeBreakpoint, CodexApiResponse, CompilationResult, CompilationResultDetail } from "../types/compiler";
-
-const BREAKPOINT_BEGIN_TAG = "<algodebug-breakpoint";
-const BREAKPOINT_END_TAG = "</algodebug-breakpoint>\n";
-
-const VARIABLE_BEGIN_TAG = "<algodebug-variable";
-const VARIABLE_END_TAG = "</algodebug-variable>\n";
+import { CodexApiResponse, CompilationResult, CompilationResultDetail } from "../types/compiler";
+import { OutputParser } from "../utils/OutputParser";
 
 export const parseOutput = (outputs: CodexApiResponse[]) => {
     let result: CompilationResult = {
@@ -18,28 +13,8 @@ export const parseOutput = (outputs: CodexApiResponse[]) => {
             let detail: CompilationResultDetail = {
                 success: true,
                 errorMessage: null,
-                output: { fullOutput: out.output, partialOutputs: [], frames: [] },
+                output: new OutputParser(out.output).parse(),
             };
-
-            // parse output
-            let breakpointStart = out.output.indexOf(BREAKPOINT_BEGIN_TAG);
-            let breakpointEnd = 0;
-            let id = 0;
-
-            while (breakpointStart !== -1) {
-                detail.output.partialOutputs.push(out.output.substring(breakpointEnd, breakpointStart));
-
-                breakpointEnd = out.output.indexOf(BREAKPOINT_END_TAG, breakpointStart) + BREAKPOINT_END_TAG.length;
-
-                // extract the line number and variables from the breakpoint
-                const breakpoint = parseBreakpoint(out.output.substring(breakpointStart, breakpointEnd), id);
-                detail.output.frames.push(breakpoint);
-                id += 1;
-
-                breakpointStart = out.output.indexOf(BREAKPOINT_BEGIN_TAG, breakpointEnd);
-            }
-            // add the last partial output
-            detail.output.partialOutputs.push(out.output.substring(breakpointEnd));
 
             result.details.push(detail);
         } else {
@@ -52,38 +27,4 @@ export const parseOutput = (outputs: CodexApiResponse[]) => {
         }
     }
     return result;
-};
-
-const parseBreakpoint = (output: string, id: number): CodeBreakpoint => {
-    // extract the line number
-    const lineStart = output.indexOf("line=") + 6;
-    const lineEnd = output.indexOf('"', lineStart);
-    const line = parseInt(output.substring(lineStart, lineEnd));
-
-    // extract the variables
-    let variables: Record<string, string> = {};
-
-    let variableStart = output.indexOf(VARIABLE_BEGIN_TAG);
-    let variableEnd = 0;
-
-    while (variableStart !== -1) {
-        variableEnd = output.indexOf(VARIABLE_END_TAG, variableStart) + VARIABLE_END_TAG.length;
-
-        const variableTag = output.substring(variableStart, variableEnd);
-        const [name, value] = parseVariable(variableTag);
-        variables[name] = value;
-
-        variableStart = output.indexOf(VARIABLE_BEGIN_TAG, variableEnd);
-    }
-    return { id, line: line, variables: variables };
-};
-
-const parseVariable = (output: string): [string, string] => {
-    const variableNameStart = output.indexOf("name=") + 6;
-    const variableNameEnd = output.indexOf('"', variableNameStart);
-    const variableName = output.substring(variableNameStart, variableNameEnd);
-
-    const variableValue = output.substring(output.indexOf(">") + 1, output.indexOf("<", output.indexOf(">")));
-
-    return [variableName, variableValue];
 };
---
+++ backend/src/utils/OutputParser.ts
@@ -0,0 +1,82 @@
+import { CodeBreakpoint, CodeOutput } from "../types/compiler";
+
+const BREAKPOINT_TAG = "algodebug-breakpoint";
+const VARIABLE_TAG = "algodebug-variable";
+
+const BREAKPOINT_BEGIN_TAG = "<algodebug-breakpoint";
+const BREAKPOINT_END_TAG = "</algodebug-breakpoint>\n";
+
+type Breakpoint = {
+    tag: string;
+    start: number;
+    end: number;
+};
+
+export class OutputParser {
+    output: string = "";
+    position: number = 0;
+
+    constructor(output: string) {
+        this.output = output;
+    }
+
+    parse(): CodeOutput {
+        let result: CodeOutput = {
+            fullOutput: this.output,
+            partialOutputs: [],
+            frames: [],
+        };
+
+        do {
+            var [found, breakpoint] = this.findNextBreakpoint();
+            if (found && breakpoint) {
+                result.partialOutputs.push(this.output.substring(this.position, breakpoint.start));
+                result.frames.push(this.parseBreakpoint(breakpoint));
+                this.position = breakpoint.end;
+            }
+        } while (found);
+
+        return result;
+    }
+
+    findNextBreakpoint(): [true, Breakpoint] | [false] {
+        let breakpointStart = this.output.indexOf(BREAKPOINT_BEGIN_TAG, this.position);
+        let breakpointEnd = this.output.indexOf(BREAKPOINT_END_TAG, this.position);
+        if (breakpointStart === -1 || breakpointEnd === -1) {
+            return [false];
+        }
+
+        let breakpointTag = this.output.substring(breakpointStart, breakpointEnd);
+        let breakpoint = {
+            tag: breakpointTag,
+            start: breakpointStart,
+            end: breakpointEnd + BREAKPOINT_END_TAG.length,
+        };
+
+        return [true, breakpoint];
+    }
+
+    parseBreakpoint(breakpoint: Breakpoint): CodeBreakpoint {
+        const parser = new DOMParser();
+        const parsedBreakpoint = parser
+            .parseFromString(breakpoint.tag, "text/html")
+            .getElementsByTagName(BREAKPOINT_TAG)[0];
+
+        let line = Number(parsedBreakpoint.getAttribute("line"));
+
+        let variables: Record<string, string> = {};
+        for (let variable of parsedBreakpoint.getElementsByTagName(VARIABLE_TAG)) {
+            let variableName = variable.getAttribute("name");
+            let variableValue = variable.innerHTML;
+            if (variableName) {
+                variables[variableName] = variableValue;
+            }
+        }
+
+        return { id: 0, line, variables };
+    }
+
+    parseVariable(variable: Element) {
+        return [variable.getAttribute("name"), variable.innerHTML];
+    }
+}
