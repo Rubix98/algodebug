c523c5e7ab0ed3ec50b98dbacc1e95b277565c06
Jakub Pawłowski <59766830+pawl0wski@users.noreply.github.com>
2022-12-08 22:04:33 +0100

    feat(frontend): Install Vuetify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,25 @@ import Vue3Sanitize from "vue-3-sanitize";
 import VueKonva from "vue-konva";
 import store from "@/store";
 
+// Vuetify
+import "vuetify/styles";
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+        }
+    },
+});
+
 import "@/javascript/prototypes/Map";
 import "@/javascript/prototypes/String";
 
@@ -14,4 +33,5 @@ app.config.globalProperties.emitter = mitt();
 app.use(Vue3Sanitize);
 app.use(VueKonva);
 app.use(store);
+app.use(vuetify);
 app.mount("#app");
