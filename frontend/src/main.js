dfd61acc2561ce32d92df11b934b006b1183c0a6
xenix1337 <xenix1337gaming@gmail.com>
2022-12-08 16:19:13 +0100

    Installed Toast library and registered it

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,8 @@ import mitt from "mitt";
 import Vue3Sanitize from "vue-3-sanitize";
 import VueKonva from "vue-konva";
 import store from "@/store";
+import Toast, { POSITION } from "vue-toastification";
+import "vue-toastification/dist/index.css";
 
 import "@/javascript/prototypes/Map";
 import "@/javascript/prototypes/String";
@@ -14,4 +16,8 @@ app.config.globalProperties.emitter = mitt();
 app.use(Vue3Sanitize);
 app.use(VueKonva);
 app.use(store);
+app.use(Toast, {
+    position: POSITION.TOP_RIGHT,
+    timeout: 3000,
+});
 app.mount("#app");
