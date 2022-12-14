f867922930cad3bd6f73e9d2f20092c8ebf755d7
Marcin Wojdat <wojdatm98@gmail.com>
2022-12-14 19:37:46 +0100

    Add .env files to repository

--- frontend/src/javascript/utils/axiosUtils.js
+++ frontend/src/javascript/utils/axiosUtils.js
@@ -4,8 +4,9 @@ export function sendRequest(url, data = {}, method) {
     if (!validateMethod(method)) return;
 
     method = method.toLowerCase();
-    url = getBackendUrl() + url;
+    url = process.env.VUE_APP_BACKEND_URL + url;
     console.log(method, url);
+
     return axios[method](url, data)
         .then((response) => {
             console.log(response);
@@ -17,10 +18,6 @@ export function sendRequest(url, data = {}, method) {
                 error.message + (error.response.data.error ? "\nDetails: " + error.response.data.error : "");
             alert(errorMessage);
         });
-}
-
-function getBackendUrl() {
-    return window.location.origin.includes("localhost") ? "http://localhost:8080" : "https://algodebug.herokuapp.com";
 }
 
 function validateMethod(method) {
