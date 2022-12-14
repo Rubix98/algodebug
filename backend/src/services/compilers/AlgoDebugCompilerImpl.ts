ebef2079ab81c9ddfff886e46f6aacd52c7c4d31
Marcin Wojdat <wojdatm98@gmail.com>
2022-12-14 19:35:09 +0100

    Add AlgoDebugCompiler

---
+++ backend/src/services/compilers/AlgoDebugCompilerImpl.ts
@@ -0,0 +1,43 @@
+import { CompilerRequest, CompilerResponse } from "../../types/compiler";
+import { OutputParser } from "../../utils/OutputParser";
+import { Compiler } from "./compilerFactory";
+
+export class AlgoDebugCompilerImpl implements Compiler {
+    static API_URL: string = "http://srv16.mikr.us:40042/compile";
+
+    async compile(request: CompilerRequest): Promise<CompilerResponse> {
+        const apiResponse = await fetch(AlgoDebugCompilerImpl.API_URL, {
+            method: "POST",
+            body: JSON.stringify({
+                code: request.code,
+                input: request.input,
+                language: request.language,
+            }),
+            headers: { "Content-Type": "application/json" },
+        });
+        const response: CodexApiResponse = await apiResponse.json();
+
+        if (response.success) {
+            return {
+                success: true,
+                output: new OutputParser(response.output).parse(),
+                errorMessage: null,
+            };
+        } else {
+            return {
+                success: false,
+                output: null,
+                errorMessage: response.errorMessage,
+            };
+        }
+    }
+}
+
+//prettier-ignore
+type CodexApiResponse = {
+    success: true;
+    output: string;
+} | {
+    success: false;
+    errorMessage: string;
+};
