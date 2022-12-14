9367870532155f7830ef17e5ea96913d8c692cda
xenix1337 <xenix1337gaming@gmail.com>
2022-12-14 10:28:52 +0100

    Fixed bug with applying empty subobjects while saving

--- frontend/src/javascript/utils/validationUtils.js
+++ frontend/src/javascript/utils/validationUtils.js
@@ -3,10 +3,12 @@ import toast from "@/javascript/utils/toastUtils";
 export function validateSceneObject(model) {
     if (!assert(model.type != null, "Podaj rodzaj obiektu")) return false;
     if (!assert(model.variable != null, "Przypisz zmienną")) return false;
+    let problemWithSubobject = false;
     model.subobjects.forEach((subObj) => {
-        if (!assert(subObj.type != null, "Podaj rodzaj właściwości")) return false;
-        if (!assert(subObj.variable != null, "Przypisz zmienną do właściwości")) return false;
+        if (!assert(subObj.type != null, "Podaj rodzaj właściwości")) return (problemWithSubobject = true);
+        if (!assert(subObj.variable != null, "Przypisz zmienną do właściwości")) return (problemWithSubobject = true);
     });
+    if (problemWithSubobject) return false;
     return true;
 }
 
