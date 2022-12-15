ed83dfc344ba55dbab69ae9ab1894e0606ba49e7
Sobgo <60814050+Sobgo@users.noreply.github.com>
2022-12-15 22:31:30 +0100

    fix tests
    because when tests fail it means you need to fix tests

--- backend/tests/validation.test.ts
+++ backend/tests/validation.test.ts
@@ -1,4 +1,4 @@
-import { validateConverter, validateProject } from "../src/services/dbservice";
+import { validate } from "../src/services/dbservice";
 import { Converter } from "../src/models/Converter";
 import { Project } from "../src/models/Project";
 import { Breakpoint } from "../src/structures/Breakpoint";
@@ -7,24 +7,25 @@ import { Mark } from "../src/structures/Mark";
 import { ObjectType } from "../src/structures/ObjectType";
 import { SceneObject } from "../src/structures/SceneObject";
 import { TestCase } from "../src/structures/TestCase";
-import { sanitizeConverter } from "../src/models/Converter";
+import { sanitize as sanitizeConverter } from "../src/models/Converter";
+import { rejects } from "assert";
 
-const checkProject = (o: unknown) => {
+const checkProject = async (o: unknown) => {
     // might throw an error
     Project.check(o);
 
-    const [isOk, project] = validateProject(o);
+    const [isOk, project] = await validate(o, Project, "Project");
     if (!isOk) {
         return { ok: false, error: project };
     }
     return { ok: true, value: project };
 };
 
-const checkConverter = (o: unknown) => {
+const checkConverter = async (o: unknown) => {
     // might throw an error
     Converter.check(o);
 
-    const [isOk, converter] = validateConverter(o);
+    const [isOk, converter] = await validate(o, Converter, "Converter");
     if (!isOk) {
         return { ok: false, error: converter };
     }
@@ -77,13 +78,13 @@ let validNestedProject = {
 // some jest unit tests for jsons that can be recived by API
 // this only tests validation functions, not the API itself
 describe("Project validation", () => {
-    test("Valid project", () => {
-        expect(checkProject(validProject).ok).toBe(true);
+    test("Valid project", async () => {
+        expect((await checkProject(validProject)).ok).toBe(true);
     });
 
-    test("Additional property", () => {
+    test("Additional property", async () => {
         // expect object to pass check and equal validProject
-        const checked = checkProject({ ...validProject, additional: "property" });
+        const checked = await checkProject({ ...validProject, additional: "property" });
         expect(checked.ok).toBe(true);
         // ignore the difference in creation and modification date
         expect(checked.value).toEqual({
@@ -95,62 +96,63 @@ describe("Project validation", () => {
 
     test('Addtional property named "constructor" (edge case)', () => {
         // TODO more descriptive error message OR fix edge case
-        expect(() => checkProject({ ...validProject, constructor: "property" })).toThrow();
+        expect(async () => await checkProject({ ...validProject, constructor: "property" })).rejects.toThrow();
     });
 
     test("Missing property", () => {
-        expect(() => checkProject({ ...validProject, title: undefined })).toThrow();
+        expect(async () => (await checkProject({ ...validProject, title: undefined }))).rejects.toThrow();
     });
 
     test("Empty title", () => {
-        expect(() => checkProject({ ...validProject, title: "" })).toThrow();
+        expect(async () => (await checkProject({ ...validProject, title: "" }))).rejects.toThrow();
     });
 
     test("Empty object", () => {
-        expect(() => checkProject({})).toThrow();
+        expect(async () => (await checkProject({}))).rejects.toThrow();
     });
 
     test("Date in request", () => {
         // we expect check to throw an error
         // because we don't want to allow users to set creationDate and modificationDate
         // and there is no way to pass Date object through json (only primitive types)
-        expect(() => checkProject({ ...validProject, creationDate: "01.01.2022" })).toThrow();
-        expect(() => checkProject({ ...validProject, modificationDate: new Date().toString() })).toThrow();
-        expect(() => checkProject({ ...validProject, creationDate: 1234 })).toThrow();
-        expect(() => checkProject({ ...validProject, modificationDate: null })).toThrow();
+        expect(async () => (await checkProject({ ...validProject, creationDate: "01.01.2022" }))).rejects.toThrow();
+        expect(async () => (await checkProject({ ...validProject, modificationDate: new Date().toString() }))).rejects.toThrow();
+        expect(async () => (await checkProject({ ...validProject, creationDate: 1234 }))).rejects.toThrow();
+        expect(async () => (await checkProject({ ...validProject, modificationDate: null }))).rejects.toThrow();
 
         // this should pass
-        expect(() => checkProject({ ...validProject, creationDate: new Date("01.01.2022") })).not.toThrow();
-        expect(() => checkProject({ ...validProject, modificationDate: new Date() })).not.toThrow();
+        expect(async () => (await checkProject({ ...validProject, creationDate: new Date("01.01.2022") }))).not.toThrow();
+        expect(async () => (await checkProject({ ...validProject, modificationDate: new Date() }))).not.toThrow();
     });
 
-    test("Adding date", () => {
+    test("Adding date", async () => {
         // check if date is added and is of type Date
-        let checked = checkProject({ ...validProject, creationDate: undefined });
+        let checked = await checkProject({ ...validProject, creationDate: undefined });
         expect(checked.ok).toBe(true);
         if (checked.value) expect(checked.value.creationDate).toBeInstanceOf(Date);
 
-        checked = checkProject({ ...validProject, modificationDate: undefined });
+        checked = await checkProject({ ...validProject, modificationDate: undefined });
         expect(checked.ok).toBe(true);
         if (checked.value) expect(checked.value.modificationDate).toBeInstanceOf(Date);
     });
 
-    test("Valid project with nested objects", () => {
-        expect(checkProject(validNestedProject).ok).toBe(true);
+    test("Valid project with nested objects", async () => {
+        expect((await checkProject(validNestedProject)).ok).toBe(true);
     });
 
     test("Invalid nested object", () => {
-        expect(() =>
-            checkProject({
-                ...validNestedProject,
-                sceneObjects: [{ ...validNestedProject.sceneObjects[0], type: "invalid" }],
-            })
-        ).toThrow();
+        expect(
+            async () =>
+                await checkProject({
+                    ...validNestedProject,
+                    sceneObjects: [{ ...validNestedProject.sceneObjects[0], type: "invalid" }],
+                })
+        ).rejects.toThrow();
     });
 
-    test("Nested object with additional property", () => {
+    test("Nested object with additional property", async () => {
         // expect object to pass check and equal validNestedProject
-        const checked = checkProject({
+        const checked = await checkProject({
             ...validNestedProject,
             breakpoints: [
                 { id: 1, additional: "property" } as Breakpoint,
@@ -169,55 +171,55 @@ describe("Project validation", () => {
 
     // in POST id is only hint but will not override existing object
     // but specifiying id will be required in PUT to update existing object
-    test("Id present", () => {
-        expect(() => checkProject({ ...validProject, _id: null })).toThrow();
-        expect(() => checkProject({ ...validProject, _id: "1234" })).toThrow();
+    test("Id present", async () => {
+        expect(async () => await checkProject({ ...validProject, _id: null })).rejects.toThrow();
+        expect(async () => await checkProject({ ...validProject, _id: "1234" })).rejects.toThrow();
 
         // expect to pass check
-        expect(() => checkProject({ ...validProject, _id: 1 })).not.toThrow();
-        expect(checkProject({ ...validProject, _id: 1 }).ok).toBe(true);
-        expect(() => checkProject({ ...validProject, _id: "0123456789abcdef01234567" })).not.toThrow();
-        expect(checkProject({ ...validProject, _id: "0123456789abcdef01234567" }).ok).toBe(true);
+        expect(async () => await checkProject({ ...validProject, _id: 1 })).not.toThrow();
+        expect((await checkProject({ ...validProject, _id: 1 })).ok).toBe(true);
+        expect(async () => await checkProject({ ...validProject, _id: "0123456789abcdef01234567" })).not.toThrow();
+        expect((await checkProject({ ...validProject, _id: "0123456789abcdef01234567" })).ok).toBe(true);
     });
 });
 
 // some converter tests are done in project tests because they are nested in project
 describe("Converter validation", () => {
-    test("Valid converter", () => {
-        expect(checkConverter(validConverter).ok).toBe(true);
+    test("Valid converter", async () => {
+        expect((await checkConverter(validConverter)).ok).toBe(true);
     });
 
-    test("Additional property", () => {
+    test("Additional property", async () => {
         // expect object to pass check and equal validConverter
-        const checked = checkConverter({ ...validConverter, additional: "property" });
+        const checked = await checkConverter({ ...validConverter, additional: "property" });
         expect(checked.ok).toBe(true);
         expect(checked.value).toEqual(validConverter);
     });
 
     test("Missing property", () => {
-        expect(() => checkConverter({ ...validConverter, title: undefined })).toThrow();
+        expect(async () => await checkConverter({ ...validConverter, title: undefined })).rejects.toThrow();
     });
 
     test("Empty object", () => {
-        expect(() => checkConverter({})).toThrow();
+        expect(async () => await checkConverter({})).rejects.toThrow();
     });
 
     test("Null instead of converter", () => {
-        expect(() => checkConverter(null)).toThrow();
+        expect(async () => await checkConverter(null)).rejects.toThrow();
         // but this should pass (because in project converter is optional)
         expect(sanitizeConverter(null)).toEqual(null);
     });
 
     // in POST id is only hint but will not override existing object
     // but specifiying id will be required in PUT to update existing object
-    test("Id present", () => {
-        expect(() => checkConverter({ ...validConverter, _id: null })).toThrow();
-        expect(() => checkConverter({ ...validConverter, _id: "1234" })).toThrow();
+    test("Id present", async () => {
+        expect(async () => await checkConverter({ ...validConverter, _id: null })).rejects.toThrow();
+        expect(async () => await checkConverter({ ...validConverter, _id: "1234" })).rejects.toThrow();
 
         // expect to pass check
-        expect(() => checkConverter({ ...validConverter, _id: 1 })).not.toThrow();
-        expect(checkConverter({ ...validConverter, _id: 1 }).ok).toBe(true);
-        expect(() => checkConverter({ ...validConverter, _id: "0123456789abcdef01234567" })).not.toThrow();
-        expect(checkConverter({ ...validConverter, _id: "0123456789abcdef01234567" }).ok).toBe(true);
+        expect(async () => await checkConverter({ ...validConverter, _id: 1 })).not.toThrow();
+        expect((await checkConverter({ ...validConverter, _id: 1 })).ok).toBe(true);
+        expect(async () => await checkConverter({ ...validConverter, _id: "0123456789abcdef01234567" })).not.toThrow();
+        expect((await checkConverter({ ...validConverter, _id: "0123456789abcdef01234567" })).ok).toBe(true);
     });
 });
