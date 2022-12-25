ac0583e563f3493b4659039403f763ecdc43bf82
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-12-25 20:46:53 +0100

    fix(frontend): Fix close button in AlgoModal

--- frontend/src/components/global/AlgoModal.vue
+++ frontend/src/components/global/AlgoModal.vue
@@ -3,7 +3,7 @@
         <v-card-title class="dialog__title">
             <h2>{{ title }}</h2>
             <v-spacer />
-            <v-btn class="dialog__title__close" elevation="0" icon>
+            <v-btn class="dialog__title__close" elevation="0" icon @click="popModal">
                 <v-icon size="24"> mdi-close </v-icon>
             </v-btn>
         </v-card-title>
@@ -15,7 +15,7 @@
         <v-divider />
         <v-card-actions>
             <v-spacer />
-            <AlgoButton @click="popModal()">Anuluj</AlgoButton>
+            <AlgoButton @click="popModal">Anuluj</AlgoButton>
             <slot name="buttons"></slot>
         </v-card-actions>
     </v-card>
