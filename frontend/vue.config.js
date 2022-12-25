b61d2e23b92d3d53e01c2038de7c979a0a359e3d
xenix1337 <xenix1337gaming@gmail.com>
2022-12-25 01:51:13 +0100

    Revert trying to load a plugin - it fatally fails

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,11 +1,6 @@
-const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
-
 module.exports = {
     publicPath: "/",
     devServer: {
         allowedHosts: ["srv16.mikr.us"],
-    },
-    configureWebpack: {
-        plugins: [new MonacoWebpackPlugin()],
     },
 };
