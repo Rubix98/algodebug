a3b798375cdbece2e36df9aa5adc859f252e456f
xenix1337 <xenix1337gaming@gmail.com>
2023-02-22 00:18:56 +0100

    Added footer with custom modal. Had to change AlgoModal a bit.

---
+++ frontend/src/components/interface/drawer/NavigationFooter.vue
@@ -0,0 +1,36 @@
+<template>
+    <footer>
+        <v-divider />
+        <div class="footerContent">
+            AlgoDebug<sup>&#169;</sup> &#9679; <a href="#" @click="showDevelopersModal()">Autorzy</a>
+        </div>
+    </footer>
+</template>
+
+<script>
+    import { defineComponent } from "vue";
+    import { pushModal } from "jenesius-vue-modal";
+    import DevelopersModal from "@/components/modals/other/DevelopersModal.vue";
+
+    export default defineComponent({
+        name: "NavigationDrawer",
+        methods: {
+            showDevelopersModal() {
+                pushModal(DevelopersModal);
+            },
+        },
+    });
+</script>
+
+<style scoped>
+    footer {
+        position: absolute;
+        width: 100%;
+        bottom: 0px;
+    }
+
+    .footerContent {
+        padding: 5px;
+        text-align: center;
+    }
+</style>
---
+++ frontend/src/components/modals/other/DevelopersModal.vue
@@ -0,0 +1,27 @@
+<template>
+    <AlgoModal title="Drużyna AlgoDebug" closeButtonLabel="Zamknij">
+        <ul>
+            <li>Marcin Wojdat - CEO</li>
+            <li>Jakub Pawłowski - Pracownik miesiąca styczeń 2023</li>
+            <li>Łukasz Skabowski - Pracownik miesiąca grudzień 2022</li>
+            <li>Mateusz Sobkowiak - Backend</li>
+            <li>Kamil Ruszkowski</li>
+            <li>Paweł Aniszewski</li>
+            <li>Paweł Czarkowski</li>
+            <br />
+            <v-divider />
+            <br />
+            <div><b>Kontakt: </b><a href="mailto:support@algodebug.pl">support@algodebug.pl</a></div>
+            <br />
+        </ul>
+    </AlgoModal>
+</template>
+
+<script>
+    import { defineComponent } from "vue";
+    import AlgoModal from "@/components/global/AlgoModal.vue";
+
+    export default defineComponent({
+        components: { AlgoModal },
+    });
+</script>
