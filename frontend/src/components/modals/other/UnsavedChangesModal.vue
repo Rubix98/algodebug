64b4fd375ba6a9bc41bdc9046d578eb5cd1fb9c5
xenix1337 <xenix1337gaming@gmail.com>
2023-02-28 02:01:56 +0100

    Added "Are you sure?" prompt

---
+++ frontend/src/components/modals/other/UnsavedChangesModal.vue
@@ -0,0 +1,35 @@
+<template>
+    <AlgoModal title="Niezapisane zmiany" :cantClose="true">
+        Masz niezapisane zmiany. Czy na pewno zamknąć?
+        <template #buttons>
+            <v-btn @click="close()" color="#ff0000">Tak</v-btn>
+            <v-btn @click="dontClose()" color="primary">Nie</v-btn>
+        </template>
+    </AlgoModal>
+</template>
+
+<script>
+    import { defineComponent } from "vue";
+    import AlgoModal from "@/components/global/AlgoModal.vue";
+    import { Modal, onBeforeModalClose } from "jenesius-vue-modal";
+
+    export default defineComponent({
+        components: { AlgoModal },
+
+        setup() {
+            onBeforeModalClose((e) => {
+                if (e.esc || e.background) return false;
+            });
+        },
+
+        methods: {
+            close() {
+                this.$emit(Modal.EVENT_PROMPT, true);
+            },
+
+            dontClose() {
+                this.$emit(Modal.EVENT_PROMPT, false);
+            },
+        },
+    });
+</script>
