8707a1965bf967596d16d7773a6d96636b2a7322
xenix1337 <xenix1337gaming@gmail.com>
2023-03-30 07:32:23 +0200

    Removed changes from AlgoModal, since they were meant to be only in PickVariableModal

--- frontend/src/components/global/AlgoModal.vue
+++ frontend/src/components/global/AlgoModal.vue
@@ -52,9 +52,6 @@
         border-width: 1px;
         border-color: $border-color-root;
 
-        display: flex;
-        flex-direction: column;
-
         &__content {
             padding: 1rem 2rem;
             max-height: 65vh;
