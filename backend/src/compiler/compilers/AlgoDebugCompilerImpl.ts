cbd4ac8aa71058eacacd461141e52436a888eb87
Marcin Wojdat <wojdatm98@gmail.com>
2023-04-02 20:30:57 +0200

    Updates domain data in env files

--- backend/src/compiler/compilers/AlgoDebugCompilerImpl.ts
+++ backend/src/compiler/compilers/AlgoDebugCompilerImpl.ts
@@ -3,7 +3,7 @@ import { OutputParser } from "../utils/OutputParser";
 import { Compiler } from "./compilerFactory";
 
 export class AlgoDebugCompilerImpl implements Compiler {
-    static API_URL: string = "http://srv16.mikr.us:40042/compile";
+    static API_URL: string = "https://compiler.algodebug.pl/compile";
 
     async compile(request: CompilerRequest): Promise<CompilerResponse> {
         const url = process.env.COMPILER_API_URL ?? AlgoDebugCompilerImpl.API_URL;
