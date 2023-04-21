4fa158ff8602cbeb94395e034a9d3e524dba2d1c
Jakub Pawłowski <59766830+pawl0wski@users.noreply.github.com>
2023-04-21 23:31:22 +0200

    Show options to overwrite projects only if the user is the author

--- frontend/src/components/modals/menu/SaveProjectModal.vue
+++ frontend/src/components/modals/menu/SaveProjectModal.vue
@@ -1,10 +1,9 @@
 <template>
     <AlgoModal title="Zapisz projekt">
         <v-text-field label="Tytuł projektu" v-model="newTitle" clearable />
-
         <template #buttons>
             <v-btn color="primary" @click="save(false)">Zapisz jako</v-btn>
-            <v-btn color="primary" @click="save(true)" v-if="this._id">Zapisz</v-btn>
+            <v-btn color="primary" @click="save(true)" v-if="this.showOverrideOption">Zapisz</v-btn>
         </template>
     </AlgoModal>
 </template>
@@ -14,7 +13,8 @@
     import { closeModal } from "jenesius-vue-modal";
     import { defineComponent } from "vue";
     import { useProjectStore } from "@/stores/project";
-    import { mapActions, mapWritableState } from "pinia";
+    import {mapActions, mapState} from "pinia";
+    import {useUserStore} from "@/stores/user";
 
     export default defineComponent({
         components: { AlgoModal },
@@ -39,7 +39,12 @@
         },
 
         computed: {
-            ...mapWritableState(useProjectStore, ["title", "_id"]),
+            ...mapState(useProjectStore, ["title", "_id", "authorId"]),
+            ...mapState(useUserStore, ["user"]),
+
+            showOverrideOption() {
+                return this._id && this.authorId === this.user._id;
+            }
         },
     });
 </script>
