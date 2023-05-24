5caa62e6968da720615e0558b7e6ca86dd011369
pwaczar <pwaczar@gmail.com>
2023-05-24 10:57:43 +0200

    Poprawki

--- frontend/src/javascript/utils/shortcutsUtils.js
+++ frontend/src/javascript/utils/shortcutsUtils.js
@@ -3,54 +3,53 @@ import { useProjectStore } from "@/stores/project";
 export function handleShortcuts(event, emitter) {
     let handled = false;
     if (event.type == "keydown") {
-        if (event.ctrlKey && event.key === "s") {
-            useProjectStore().saveProject(useProjectStore().title, true)
+        let key = event.key.toLowerCase();
+        let caps = event.getModifierState("CapsLock");
+        let shift = event.getModifierState("Shift");
+        if (caps) shift = !shift;
+        let ctrl = event.getModifierState("Control");
+        if (!shift && ctrl && key === "s") {
+            useProjectStore().saveProject(useProjectStore().title, true);
             handled = true;
-        } else if (event.shiftKey && event.ctrlKey && event.key === "S") {
+        } else if (shift && ctrl && key === "s") {
             emitter.emit("open-save-project-modal-shortcut");
             handled = true;
-        } else if (event.ctrlKey && event.key === "ArrowLeft") {
+        } else if (!shift && ctrl && key === "arrowleft") {
             if (useProjectStore().isRunning) {
                 emitter.emit("first-frame-shortcut");
                 handled = true;
             }
-        } else if (
-            (!event.ctrlKey && event.key === "ArrowLeft") ||
-            (!event.ctrlKey && event.key === "PageUp")
-        ) {
+        } else if ((!shift && !ctrl && key === "arrowleft") || (!shift && !ctrl && key === "pageup")) {
             if (useProjectStore().isRunning) {
                 emitter.emit("previous-frame-shortcut");
                 handled = true;
             }
-        } else if (!event.ctrlKey && event.key === " ") {
+        } else if (!shift && !ctrl && key === " ") {
             emitter.emit("toggle-animation-shortcut");
             if (useProjectStore().isRunning) {
                 handled = true;
             }
-        } else if (
-            (!event.ctrlKey && event.key === "ArrowRight") ||
-            (!event.ctrlKey && event.key === "PageDown")
-        ) {
+        } else if ((!shift && !ctrl && key === "arrowright") || (!shift && !ctrl && key === "pagedown")) {
             if (useProjectStore().isRunning) {
                 emitter.emit("next-frame-shortcut");
                 handled = true;
             }
-        } else if (event.ctrlKey && event.key === "ArrowRight") {
+        } else if (!shift && ctrl && key === "arrowright") {
             if (useProjectStore().isRunning) {
                 emitter.emit("last-frame-shortcut");
                 handled = true;
             }
-        } else if (event.ctrlKey && event.key === "r") {
+        } else if (!shift && ctrl && key === "r") {
             emitter.emit("run-project-shortcut");
             handled = true;
-        } else if (event.ctrlKey && event.key === "n") {
+        } else if (!shift && ctrl && key === "n") {
             //Przeglądarka (przynajmniej moja) przechwytuje Ctrl+n więc trzeba zamienić na coś innego
             emitter.emit("create-new-project-shortcut");
             handled = true;
-        } else if (event.ctrlKey && event.key === "o") {
+        } else if (!shift && ctrl && key === "o") {
             emitter.emit("open-load-project-modal-shortcut");
             handled = true;
-        } else if (event.ctrlKey && event.key === "d") {
+        } else if (!shift && ctrl && key === "d") {
             emitter.emit("show-extended-code-shortcut");
             handled = true;
         }
